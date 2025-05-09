5b81b48bf5027d9389b92938570592632b6d0806
Parthib Gupta <77930821+ParthibGupta@users.noreply.github.com>
2025-05-10 02:43:38 +1000

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0', () => {
+app.listen(8080, '0.0.0.0', () => {
   console.log('Server running on port 8080');
 });
 
